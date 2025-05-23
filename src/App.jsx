f9399ecd376ec983580228ea5fbc8af882b6fb3d
ishika021i <ishikaj039@gmail.com>
2025-05-23 14:06:56 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,12 @@ function App() {
        }
       
        }>ADD 3 </button>
+      <button onClick ={
+       () => {
+        setvalue(0);
+       }
+      
+       }>RESET </button>
      <button onClick ={
        () => {
         setvalue(value -3);
